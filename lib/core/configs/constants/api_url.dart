af8ae2db1e739b4ab68fca0f3b0bdd2131d7eaab
andy <andy@futurmap.com>
2025-04-28 11:05:12 +0300

    add todo list file and constant url api

---
+++ lib/core/configs/constants/api_url.dart
@@ -0,0 +1,5 @@
+class ApiUrl {
+  static const String baseUrl = 'http://10.13.106.130:3000/';
+  static const String apiVersion = 'api/v1';
+  static const String signup = '$apiVersion/auth/signup';
+}
